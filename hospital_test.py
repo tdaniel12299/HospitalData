a73929ccfa5c69d93d765a1823d8571151b80723
Tyler D <130164977+tdaniel12299@users.noreply.github.com>
2026-05-03 20:49:31 -0400

    fixed bins for chi test so ratings arent effected, added ANOVA test

--- hospital_test.py
+++ hospital_test.py
@@ -2,3 +2,27 @@ import pandas as pd
 import numpy as np
 from scipy import stats
 from scipy.stats import f_oneway, ttest_rel, ttest_ind, chi2_contingency
+
+df = pd.read_csv('cleanedData.csv')
+
+#rating bins for chi test, cut() segments and sorts data into the bins 
+df['Overall Rating_Bin'] = pd.cut(df['Overall Rating'],
+                              bins= [1,2,3,4,5],
+                              labels=['Low (1-2)','Medium (3)','High (4)','Best (5)'])
+
+
+print("ANOVA test:")
+
+state_groups = [ group['Overall Rating'].dropna().values
+                for name, group in df.groupby('State')
+                if len(group['Overall Rating']) >= 3]
+
+if len(state_groups) >= 2:
+    f_stat, p_val = f_oneway(*state_groups)
+    print(f'pvalue: {p_val:.10f}') #can be displayed with p_val:.2e to show how small the value is
+
+
+
+
+
+
