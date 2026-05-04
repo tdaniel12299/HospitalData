d48ff42ca6258ec93a20a787b8d8421623d1de23
Tyler D <130164977+tdaniel12299@users.noreply.github.com>
2026-05-04 00:59:14 -0400

    chi test added

--- hospital_test.py
+++ hospital_test.py
@@ -11,7 +11,7 @@ df['Overall Rating_Bin'] = pd.cut(df['Overall Rating'],
                               labels=['Low (1-2)','Medium (3)','High (4)','Best (5)'])
 
 
-print("ANOVA test:")
+print("\nANOVA test:")
 
 state_groups = [ group['Overall Rating'].dropna().values
                 for name, group in df.groupby('State')
@@ -19,11 +19,38 @@ state_groups = [ group['Overall Rating'].dropna().values
 
 if len(state_groups) >= 2:
     f_stat, p_val = f_oneway(*state_groups)
-    print(f'pvalue: {p_val:.10f}') #can be displayed with p_val:.2e to show how small the value is
+    print(f'pvalue: {p_val:.10e}') #can be displayed with p_val:.2e to show how small the value is
 
-print("Paired T-test")
+print("\nPaired T-test:")
 
+paired_data = df.dropna(subset=['Cost of Heart Attack Procedure','Cost of Heart Failure Procedure'])
+ 
+if len(paired_data)> 0:
+    t_stat, p_val = ttest_rel(paired_data['Cost of Heart Attack Procedure'], 
+                              paired_data['Cost of Heart Failure Procedure'])
+    
+    mean_diff = (paired_data['Cost of Heart Attack Procedure']-paired_data['Cost of Heart Failure Procedure']).mean()
+    print(f"mean cost difference: ${mean_diff:,.4f}")
+    print(f'pvalue: {p_val:.10e}')
 
+print('\nTwo-Sample t-test:')
 
+#statecounts order the states in descending order to then get the top two states with the most hospitals
+state_counts = df['State'].value_counts()
+top_states = state_counts.head(2).index.tolist()
 
+#if there are moe than two states topstates, map them to state 1 and 2 
+#filters rows so only the states matching state 1 and 2 are selected then select col
+if len(top_states) >= 2:
+    state1, state2 = top_states[0], top_states[1]
+    costs_state1 = df[df['State'] == state1]['Cost of Heart Attack Procedure'].dropna()
+    costs_state2 = df[df['State'] == state2]['Cost of Heart Attack Procedure'].dropna()
 
+    if len(costs_state1) > 0 and len(costs_state2) > 0:
+        t_stat, p_val = ttest_ind(costs_state1,costs_state2, equal_var=False) # tells test not to assume the states have the same varience, spread of values
+        print(f'pvalue: {p_val:.4f}')
+
+print('\nChi-square test:')
+
+contingency = pd.crosstab(df['Overall Rating_Bin'], df['State'])
+print(contingency)
